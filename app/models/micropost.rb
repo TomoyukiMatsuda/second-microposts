76767251a489ffa9f9d9171747216627096ac5ee
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-05-08 09:00:24 +0900

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   # userモデルとの関連付けによりユーザとの紐付けなしにはMicropostができない
   belongs_to :user
+  has_many :favorites
+  has_many :liked_users, through: :favorites, source: :user
 
   validates :content, presence: true, length: { maximum: 255 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship',foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :like_microposts, through: :favorites, source: :micropost
 
   # フォローする
   def follow(other_user)
@@ -30,7 +32,24 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
 
+  # フォローユーザ＋自分の投稿を取得
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+  # お気に入りする
+  def like(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  # お気に入りを外す
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  # お気に入り済かどうか確認する
+  def already_likes?(micropost)
+    self.like_microposts.include?(micropost)
   end
 end
